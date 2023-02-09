d30b1dc803d8cce8e1bbc0379908f6c48dbe704c
alexleuang <asleuang@gmail.com>
2023-02-09 13:18:35 -0600

    images

--- Main/src/main/java/com/nashss/se/picturegram/Activity/Request/GetImageRequest.java
+++ Main/src/main/java/com/nashss/se/picturegram/Activity/Request/GetImageRequest.java
@@ -1,4 +1,39 @@
 package com.nashss.se.picturegram.Activity.Request;
 
+
 public class GetImageRequest {
+    private final String imageUrl;
+
+    private GetImageRequest(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    @Override
+    public String toString() {
+        return "GetImageRequest{" +
+                "imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+
+    //CHECKSTYLE:OFF:Builder
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private String imageUrl;
+        private Builder withImageUrl(String imageUrl) {
+            this.imageUrl = imageUrl;
+            return this;
+        }
+        public GetImageRequest build() {
+            return new GetImageRequest(imageUrl);
+        }
+    }
 }
+
+
