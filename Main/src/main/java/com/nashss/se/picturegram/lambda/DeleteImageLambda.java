8e6453e9758e6b982cacf082e3b2062a62c35322
alexleuang <asleuang@gmail.com>
2023-02-14 11:28:02 -0600

    image lambda

---
+++ Main/src/main/java/com/nashss/se/picturegram/lambda/DeleteImageLambda.java
@@ -0,0 +1,28 @@
+package com.nashss.se.picturegram.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.nashss.se.picturegram.Activity.Request.DeleteImageRequest;
+import com.nashss.se.picturegram.Activity.Results.DeleteImageResult;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class DeleteImageLambda extends LambdaActivityRunner<DeleteImageRequest, DeleteImageResult>
+    implements RequestHandler<LambdaRequest<DeleteImageRequest>, LambdaResponse> {
+
+    private final Logger log = LogManager.getLogger();
+
+    @Override
+    public LambdaResponse handleRequest(LambdaRequest<DeleteImageRequest> input, Context context) {
+        log.info("handleRequest");
+
+        return super.runActivity(
+                () -> input.fromPath(path ->
+                        DeleteImageRequest.builder()
+                                .withImageUrl(path.get("Image"))
+                                .build()),
+                (request, serviceComponent) ->
+                        serviceComponent.provideDeleteImage().handleRequest(request)
+        );
+    }
+}
